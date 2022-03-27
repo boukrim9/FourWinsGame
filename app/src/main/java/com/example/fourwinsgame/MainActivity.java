ea8912b8d6c255c9c4feae74b2767596b0f8675e
Taoufik <toufikboukrim@gmail.com>
2022-03-27 10:10:25 +0000

    drop the token to first empty row

--- app/src/main/java/com/example/fourwinsgame/MainActivity.java
+++ app/src/main/java/com/example/fourwinsgame/MainActivity.java
@@ -2,6 +2,8 @@ package com.example.fourwinsgame;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.graphics.Color;
 import android.os.Bundle;
 
@@ -11,13 +13,14 @@ import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
-    private Button [] buttons = new Button[42];
+    private Button[] buttons = new Button[42];
 
-    public enum Player {Empty ,One , Two};
-    private Player activePlayer;
+    public enum Player {Empty, One, Two};
 
-    private Player [][] board = new Player [7][6];
-    boolean winner = false;
+    public Player activePlayer;
+
+    public Player [][] board = new Player[7][6];
+
 
 
 
@@ -35,13 +38,14 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
         activePlayer = Player.One;
 
-        for (int c=0; c<7; c++){
-            for (int r=0; r<6; r++){
+        for (int c = 0; c < 7; c++) {
+            for (int r = 0; r < 6; r++) {
                 board[c][r] = Player.Empty;
             }
         }
 
-}
+
+    }
 
 
     @Override
@@ -49,14 +53,10 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
 
 
-        // check if button cell is already used then exit
-        if (!((Button) view).getText().toString().equals("")){
-            return;
-        }
 
         Button button = (Button) view;
 
-        if (button==null){
+        if (button == null) {
             return;
         }
 
@@ -67,37 +67,140 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         buttonId = buttonId.substring(4);
         int buttonIndex = Integer.parseInt(buttonId);
 
-        column = buttonIndex/6;
+        column = buttonIndex / 6;
         row = buttonIndex % 6;//modulo operation gives us the rest of the division
         //row = buttonIndex - column*6;
 
+        if(board[column][row] != Player.Empty){
+            AlertDialog.Builder dlgAlert  = new AlertDialog.Builder(this);
+            dlgAlert.setMessage("spot occupied");
+            dlgAlert.setTitle("invalid move !!!");
 
-        if (activePlayer == Player.One){
+            dlgAlert.setPositiveButton("Ok",
+                    new DialogInterface.OnClickListener() {
+                        public void onClick(DialogInterface dialog, int which) {
+                            //dismiss the dialog
+                        }
+                    });
+
+            dlgAlert.setCancelable(true);
+            dlgAlert.create().show();
+
+            return;
+        }
+
+
+        while (row < 5 && board[column][row + 1] == Player.Empty){
+            row++;
+        }
+
+        buttonIndex = column * 6 + row;
+        button = buttons[buttonIndex];
+
+
+
+
+
+
+
+
+
+
+
+        if (activePlayer == Player.One) {
             button.setText("X");
             button.setTextColor(Color.parseColor("#FFC34A"));
             board[column][row] = Player.One;
-        }
 
-        else if(activePlayer == Player.Two){
+        } else if (activePlayer == Player.Two) {
             button.setText("O");
             button.setTextColor(Color.parseColor("#70FFEA"));
             board[column][row] = Player.Two;
-
         }
 
+        if(checkWinner()){
+            //Toast.makeText(this, "player won !", Toast.LENGTH_SHORT).show();
+
+            AlertDialog.Builder dlgAlert  = new AlertDialog.Builder(this);
+            dlgAlert.setMessage("a player has won");
+            dlgAlert.setTitle("Game over !!!");
+
+            dlgAlert.setPositiveButton("Ok",
+                    new DialogInterface.OnClickListener() {
+                        public void onClick(DialogInterface dialog, int which) {
+                            //dismiss the dialog
+                        }
+                    });
+
+            dlgAlert.setCancelable(true);
+            dlgAlert.create().show();
 
 
+        };
 
         //switching players
-        if (activePlayer == Player.One){
+        if (activePlayer == Player.One) {
             activePlayer = Player.Two;
-        }
-
-        else if(activePlayer == Player.Two){
+        } else if (activePlayer == Player.Two) {
             activePlayer = Player.One;
         }
 
+
+
+
+
     }
+
+    public boolean checkWinner() {
+
+        //check for 4 across
+        for(int col = 0; col< board.length - 3; col++){
+            for (int row = 0;row < board[0].length;row++){
+                if (board[col][row] == activePlayer   &&
+                        board[col+1][row] == activePlayer &&
+                        board[col+2][row] == activePlayer &&
+                        board[col+3][row] == activePlayer){
+                    return true;
+                }
+            }
+        }
+        //check for 4 up and down
+        for(int col = 0; col < board.length; col++){
+            for(int row = 0; row < board[0].length - 3; row++){
+                if (board[col][row] == activePlayer   &&
+                        board[col][row+1] == activePlayer &&
+                        board[col][row+2] == activePlayer &&
+                        board[col][row+3] == activePlayer){
+                    return true;
+                }
+            }
+        }
+        //check upward diagonal
+        for(int col = 0; col < board.length - 3; col++){
+            for(int row = 3; row < board[0].length; row++){
+                if (board[col][row] == activePlayer   &&
+                        board[col+1][row-1] == activePlayer &&
+                        board[col+2][row-2] == activePlayer &&
+                        board[col+3][row-3] == activePlayer){
+                    return true;
+                }
+            }
+        }
+        //check downward diagonal
+        for(int col = 0; col < board.length - 3; col++){
+            for(int row = 0; row < board[0].length - 3; row++){
+                if (board[col][row] == activePlayer   &&
+                        board[col+1][row+1] == activePlayer &&
+                        board[col+2][row+2] == activePlayer &&
+                        board[col+3][row+3] == activePlayer){
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+
+
 
 }
 
